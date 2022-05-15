95c8156a824ba792d635d7c80da892624a9862fd
niuzhiqiang <niuzhiqiang90@foxmail.com>
2022-05-15 18:28:47 +0800

    enable all subcommand

--- cmd/root.go
+++ cmd/root.go
@@ -13,13 +13,17 @@ func NewRootCommand() *cobra.Command {
 		Use:   "yapi-user-operator",
 		Short: "yapi-user-operator is a command line tool for yapi.",
 		Long: `yapi-user-operator is a command line tool for yapi accout management.
-For more information about yapi-user-operator, please visit 
-https://github.com/niuzhiqiang90/yapi-user-operator.
+It makes your operation simple, convenient and fast.
 
-For more information about yapi, please visit https://github.com/YMFE/yapi.
+For more information about yapi-user-operator, please visit https://github.com/niuzhiqiang90/yapi-user-operator
+For more information about yapi, please visit https://github.com/YMFE/yapi
 `,
 	}
 
 	rootCmd.AddCommand(NewAddCommand())
+	rootCmd.AddCommand(NewBlockCommand())
+	rootCmd.AddCommand(NewUnBlockCommand())
+	rootCmd.AddCommand(NewDeleteCommand())
+
 	return rootCmd
 }
